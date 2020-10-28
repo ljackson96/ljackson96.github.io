89931481fbdcd35ce06adeff6ab963382ff80bf0
l.jackson96 <l.jackson96@outlook.com>
2020-10-28 14:02:11 -0500

    resume section done

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -49,56 +49,6 @@ export default function Resume() {
           {/* main-col end */}
         </div>{" "}
         {/* End Work */}
-        {/* Skills
-      ----------------------------------------------- */}
-        <div className="row skill">
-          <div className="three columns header-col">
-            <h1>
-              <span>Skills</span>
-            </h1>
-          </div>
-          <div className="nine columns main-col">
-            <p>
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-              quae ab illo inventore veritatis et quasi architecto beatae vitae
-              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-              eos qui ratione voluptatem sequi nesciunt.
-            </p>
-            <div className="bars">
-              <ul className="skills">
-                <li>
-                  <span className="bar-expand photoshop" />
-                  <em>Photoshop</em>
-                </li>
-                <li>
-                  <span className="bar-expand illustrator" />
-                  <em>Illustrator</em>
-                </li>
-                <li>
-                  <span className="bar-expand wordpress" />
-                  <em>Wordpress</em>
-                </li>
-                <li>
-                  <span className="bar-expand css" />
-                  <em>CSS</em>
-                </li>
-                <li>
-                  <span className="bar-expand html5" />
-                  <em>HTML5</em>
-                </li>
-                <li>
-                  <span className="bar-expand jquery" />
-                  <em>jQuery</em>
-                </li>
-              </ul>
-            </div>
-            {/* end skill-bars */}
-          </div>{" "}
-          {/* main-col end */}
-        </div>{" "}
-        {/* End skills */}
         {/* Education
       ----------------------------------------------- */}
         <div className="row education">
@@ -129,6 +79,49 @@ export default function Resume() {
           {/* main-col end */}
         </div>{" "}
         {/* End Education */}
+        {/* Skills
+      ----------------------------------------------- */}
+        <div className="row skill">
+          <div className="three columns header-col">
+            <h1>
+              <span>Skills</span>
+            </h1>
+          </div>
+          <div className="nine columns main-col">
+            <p> </p>{" "}
+            <div className="bars">
+              <ul className="skills">
+                <li>
+                  <span className="bar-expand photoshop" />
+                  <em>JavaScript</em>
+                </li>
+                <li>
+                  <span className="bar-expand illustrator" />
+                  <em>Ruby</em>
+                </li>
+                <li>
+                  <span className="bar-expand wordpress" />
+                  <em>Python</em>
+                </li>
+                <li>
+                  <span className="bar-expand jquery" />
+                  <em>ReactJS</em>
+                </li>
+                <li>
+                  <span className="bar-expand html5" />
+                  <em>SQL</em>
+                </li>
+                <li>
+                  <span className="bar-expand css" />
+                  <em>Rails</em>
+                </li>
+              </ul>
+            </div>
+            {/* end skill-bars */}
+          </div>{" "}
+          {/* main-col end */}
+        </div>{" "}
+        {/* End skills */}
       </section>{" "}
       {/* Resume Section End*/}
     </React.Fragment>
